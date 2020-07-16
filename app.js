ba3fe470debb5d9239bbc1cd4c6403ff8deddcea
sehermahmud <seher.mahmud@gmail.com>
2020-07-17 00:30:34 +0600

    Added more arrays

--- app.js
+++ app.js
@@ -43,46 +43,94 @@
 // const removedElements = hobbies.splice(-2, 1);
 // console.log(hobbies);
 
-const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
-// const storedResults = testResults.slice(2);
-const storedResults = testResults.concat([3.99, 2]);
+// const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
+// // const storedResults = testResults.slice(2);
+// const storedResults = testResults.concat([3.99, 2]);
 
-testResults.push(5.91);
+// testResults.push(5.91);
 
-console.log(storedResults, testResults);
-console.log(testResults.indexOf(1.5));
+// console.log(storedResults, testResults);
+// console.log(testResults.indexOf(1.5));
 
-console.log(testResults.includes(10.99));
-console.log(testResults.indexOf(10.99) !== -1);
+// console.log(testResults.includes(10.99));
+// console.log(testResults.indexOf(10.99) !== -1);
 
-const personData = [{ name: 'Seher' }, { name: 'Joita' }];
-console.log(personData.indexOf({ name: 'Joita' }));
+// const personData = [{ name: 'Max' }, { name: 'Manuel' }];
+// console.log(personData.indexOf({ name: 'Manuel' }));
 
-const joita = personData.find((person, idx, persons) => {
-  return person.name === 'Joita';
-});
+// const manuel = personData.find((person, idx, persons) => {
+//   return person.name === 'Manuel';
+// });
 
-joita.name = 'Joita';
+// manuel.name = 'Anna';
 
-console.log(joita, personData);
+// console.log(manuel, personData);
 
-const maxIndex = personData.findIndex((person, idx, persons) => {
-  return person.name === 'Seher';
-});
+// const maxIndex = personData.findIndex((person, idx, persons) => {
+//   return person.name === 'Max';
+// });
 
-console.log(maxIndex);
+// console.log(maxIndex);
+
+// const prices = [10.99, 5.99, 3.99, 6.59];
+// const tax = 0.19;
+// const taxAdjustedPrices = [];
+
+// // for (const price of prices) {
+// //   taxAdjustedPrices.push(price * (1 + tax));
+// // }
+
+// prices.forEach((price, idx, prices) => {
+//   const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
+//   taxAdjustedPrices.push(priceObj);
+// });
+
+// console.log(taxAdjustedPrices);
 
 const prices = [10.99, 5.99, 3.99, 6.59];
 const tax = 0.19;
-const taxAdjustedPrices = [];
 
-for (const price of prices) {
-  taxAdjustedPrices.push(price * (1 + tax));
-}
-
-prices.forEach((price, idx, prices) => {
+const taxAdjustedPrices = prices.map((price, idx, prices) => {
   const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
-  taxAdjustedPrices.push(priceObj);
+  return priceObj;
 });
 
-console.log(taxAdjustedPrices);
+// console.log(prices, taxAdjustedPrices);
+
+const sortedPrices = prices.sort((a, b) => {
+  if (a > b) {
+    return -1;
+  } else if (a === b) {
+    return 0;
+  } else {
+    return 1;
+  }
+});
+// console.log(sortedPrices.reverse());
+console.log(sortedPrices);
+
+const filteredArray = prices.filter(p => p > 6);
+
+console.log(filteredArray);
+
+// let sum = 0;
+
+// prices.forEach((price) => {
+//   sum += price
+// });
+
+// console.log(sum);
+
+const sum = prices.reduce((prevValue, curValue) => prevValue + curValue, 0);
+
+console.log(sum);
+
+const data = 'new york;10.99;2000';
+
+const transformedData = data.split(';');
+transformedData[1] = +transformedData[1];
+console.log(transformedData);
+
+const nameFragements = ['Seher', 'Mahmud'];
+const name = nameFragements.join(' ');
+console.log(name);
